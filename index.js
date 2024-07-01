ad739297b909e673c13a3698bd9ea5337f62add3
mauro cappabianca <maurocmiozzi@gmail.com>
2024-06-30 22:47:16 -0400

    Tercera Pre Entrega

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let option;
+/*let option;
 let total = 0;
 function calcularTax(precio, tax){
     let impuesto = precio * (tax / 100); 
@@ -33,4 +33,67 @@ while (option != 0){
     else{
         alert("Ingrese una opcion valida")
     }
+}*/
+
+const games_form = document.querySelector('#add-game');
+const games_in = document.querySelector('#games');
+const total_container = document.querySelector('#total');
+
+/*let games = JSON.parse(localStorage.getItem('games')) == null
+? localStorage.setItem('games', JSON.stringify([]))
+: JSON.parse(localStorage.getItem('games'));*/
+let games = JSON.parse(localStorage.getItem('games')) || [];
+
+const createNewGame = (game) => {
+    const old_games = JSON.parse(localStorage.getItem('games')) || [];
+    old_games.push(game);
+    localStorage.setItem('games', JSON.stringify(old_games));;
 }
+
+const appendGames = () => {
+    games_in.innerHTML = '';
+    const storedGames = JSON.parse(localStorage.getItem('games')) || [];
+    let total = 0;
+    storedGames.forEach((game, index) => {
+      const game_container = document.createElement('article');
+      game_container.className = 'game';
+      game_container.id = `game-${index}`;
+
+    game_container.innerHTML = `
+      <div>
+        <h3>${game.description}</h3>
+        <p>$${game.price}</p>
+      </div>
+      <button id="btn-${index}" class="btn-delete" type="button">Eliminar</button>
+    `;
+
+    games_in.appendChild(game_container);
+    total += parseFloat(game.price);
+    document.getElementById(`btn-${index}`).addEventListener('click', () => {
+        deleteGame(index);
+      });
+    });
+    total_container.textContent = `Total: $${total.toFixed(2)}`;
+  };
+
+  const deleteGame = (index) => {
+    let storedGames = JSON.parse(localStorage.getItem('games')) || [];
+    storedGames.splice(index, 1);
+    localStorage.setItem('games', JSON.stringify(storedGames));
+    appendGames();
+  }
+
+
+games_form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const task = {
+    description: e.target[0].value,
+    price: e.target[1].value,
+  };
+
+  createNewGame(task);
+  appendGames();
+  games_form.reset();
+});
+
+appendGames();
